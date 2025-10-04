6256d3e7ed0868c1cefd9866788a2adabf20ae9b
Koi <khoindats01044@fpt.edu.vn>
2025-10-04 09:00:32 +0700

    TS01044 Nguyễn Đặng Anh Khôi Lab 2

---
+++ Lab2/src/main/java/com/poly/lab2/controller34/Product.java
@@ -0,0 +1,25 @@
+package Lab2.controller34;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private Double price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
